e0715241d1c0b491773305e807bc04d7e095ea8f
supiraru <farhandewanta11@gmail.com>
2021-07-15 23:21:15 +0700

    Tugas 4 Looping

---
+++ Tugas-4-Looping/looping.dart
@@ -0,0 +1,83 @@
+// Soal No. 1 (Looping While)
+
+/*
+    void main(List<String> args) {
+
+        print("LOOPING PERTAMA");
+        int counter = 0;
+        while(counter < 20){
+            counter+=2;
+            print("$counter - I love coding");
+        }
+
+        print("LOOPING KEDUA");
+        while(counter > 0){
+            print("$counter - I will become a mobile developer");
+            counter-=2;
+        }
+
+    }
+*/
+
+
+//=================================================================================================
+
+
+// Soal No. 2 ( Looping menggunakan for)
+
+/*
+    void main(List<String> args) {
+
+        for(int i = 1; i <= 20; i++ ){
+            if(i%3 == 0 && i%2 == 1){
+                print("$i - I Love Coding");
+            }else if(i%2 == 1){
+                print("$i - Santai");
+            }else if (i%2 == 0){
+                print("$i - Berkualitas");
+            }
+        }
+    }
+*/
+
+//=================================================================================================
+
+
+// Soal No. 3 ( Membuat Persegi Panjang # )
+
+/*
+    void main(List<String> args) {
+
+        String result = '';
+        for(int i = 0; i < 4; i++ ){
+            for(int j = 0; j < 8; j++){
+                result += '#';
+            }
+            print(result);
+            result = '';
+        }
+    }
+*/
+
+//=================================================================================================
+
+
+// Soal No. 4 ( Membuat Tangga )
+
+
+/*
+    void main(List<String> args) {
+
+        String result = '';
+        for(int i = 1; i < 8; i++ ){
+            for(int j = 0; j < i; j++){
+                result += '#';
+            }
+            print(result);
+            result = '';
+        }
+    }
+*/
+
+//=================================================================================================
+
