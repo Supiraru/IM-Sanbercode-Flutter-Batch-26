3ad7d4a2895ddc73c51e62c3ea4da36def08d0fe
supiraru <farhandewanta11@gmail.com>
2021-07-16 21:25:34 +0700

    tugas 5 function

---
+++ Tugas-5-Function/function.dart
@@ -0,0 +1,76 @@
+// Soal No. 1 
+/*
+  void main(List<String> args) {
+    teriak();  
+  }
+
+  void teriak(){
+    print("Halo Sanbers!");
+  }
+*/
+
+
+//=================================================================================================
+
+
+// Soal No. 2 
+/*
+  void main(List<String> args) {
+    var num1 = 12;
+    var num2 = 8;
+
+    var hasilkali = kalikan(num1, num2);  
+    print(hasilkali);
+  }
+
+  int kalikan(param1, param2){
+    return param1 * param2;
+  }
+*/
+
+//=================================================================================================
+
+
+// Soal No. 3 
+
+/*
+  void main(List<String> args) {
+    var name = "agus";
+    var age = 30;
+    var address = "Jln. Malioboro, Yogyakarta";
+    var hobby = "Gaming";
+
+    var hasil = introduce(name, age, address, hobby);  
+    print(hasil);
+  }
+
+  String introduce(name, age, address, hobby){
+    return "Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby";
+  }
+*/
+
+//=================================================================================================
+
+
+// Soal No. 4 
+
+/*
+
+  void main(List<String> args) {
+    int number = 5;
+    var hasil = factorial(number);  
+    print(hasil);
+  }
+
+  int factorial(num){
+    if(num == 1 || num <= 0){
+      return 1;
+    }
+    return num*factorial(num-1);
+  }
+  
+*/
+
+//=================================================================================================
+
+
